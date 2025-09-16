f6f776a1f013103b75dcf63a4fcd65f4c14780ea
Sven-27 <github.geometric664@passinbox.com>
2025-09-16 12:55:38 +0200

    buttons are active now working on showing stats

--- script.js
+++ script.js
@@ -2,11 +2,60 @@ const timeframe = document.querySelector('.timeframe');
 const daily = document.getElementById('daily');
 const weekly = document.getElementById('weekly');
 const monthly = document.getElementById('monthly');
-const stats = document.querySelector('.stats-wrapper');
+const statsDaily = document.getElementById('stats-daily');
+const statsWeekly = document.getElementById('stats-weekly');
+const statsMonthly = document.getElementById('stats-monthly');
+const statsWrapper = document.querySelector('.stats-wrapper');
+const stats = document.querySelectorAll('.stats');
 const url = "./data.json";
 
+timeframe.addEventListener('click', (e) => {
+  const target = e.target;
+  if (target.id === 'daily') {
+    daily.classList.add('active');
+    weekly.classList.remove('active');
+    monthly.classList.remove('active');
+    stats.forEach(stat => {
+      if (stat.classList.contains('stats-daily')) {
+        stat.classList.remove('hide');
+        stat.classList.add('show');
+      } else {
+        stat.classList.remove('show');
+        stat.classList.add('hide');
+      }
+    });
+  } else if (target.id === 'weekly') {
+    daily.classList.remove('active');
+    weekly.classList.add('active');
+    monthly.classList.remove('active');
+    stats.forEach(stat => {
+      if (stat.classList.contains('stats-weekly')) {
+        stat.classList.remove('hide');
+        stat.classList.add('show');
+      }
+      else {
+        stat.classList.remove('show');
+        stat.classList.add('hide');
+      }
+    });
+  } else if (target.id === 'monthly') {
+    daily.classList.remove('active');
+    weekly.classList.remove('active');
+    monthly.classList.add('active');
+    stats.forEach(stat => {
+      if (stat.classList.contains('stats-monthly')) {
+        stat.classList.remove('hide');
+        stat.classList.add('show');
+      } else {
+        stat.classList.remove('show');
+        stat.classList.add('hide');
+      }
+    });
+  }
+});
+
 function showStats(statsData) {
-  stats.innerHTML = statsData.map((item, i) => {
+  statsWrapper.innerHTML = statsData.map((item, i) => {
     // console.log(`icon${i + 1}`)
     return `
     <div class="stats-container" >
@@ -18,19 +67,19 @@ function showStats(statsData) {
           <p class="text-preset-5-medium">${item.title}</p>
           <img src="./images/icon-ellipsis.svg" alt="ellipsis icon">
         </div>
-        <div id="stats-daily" class="stats daily">
+        <div id="stats-daily" class="stats stats-daily show">
           <p class="current text-preset-1">${item.timeframes.daily.current}hrs</p>
           <p class="previous text-preset-6">
             Yesterday - ${item.timeframes.daily.previous}hrs
           </p>
         </div>
-        <div id="stats-weekly" class="stats weekly">
+        <div id="stats-weekly" class="stats stats-weekly hide">
           <p class="current text-preset-1">${item.timeframes.weekly.current}hrs</p>
           <p class="previous text-preset-6">
             Last Week - ${item.timeframes.weekly.previous}hrs
           </p>
         </div>
-        <div id="stats-monthly" class="stats monthly">
+        <div id="stats-monthly" class="stats stats-monthly hide">
           <p class="current text-preset-1">${item.timeframes.monthly.current}hrs</p>
           <p class="previous text-preset-6">
             Last Month - ${item.timeframes.monthly.previous}hrs
